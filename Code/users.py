6b002192c07c227b15fc91b599e81a456095cb4f
Michael Hooper <mikeyhooper360@gmail.com>
2024-12-03 11:55:31 +0000

    final user changes

--- Code/users.py
+++ Code/users.py
@@ -3,50 +3,50 @@
 from flask import abort, make_response
 
 from config import db
-from models import Person, people_schema, person_schema
+from models import User, user_schema, people_schema
 
-def create(person):
-    name = person.get("name")
-    existing_person = Person.query.filter(Person.name == name).one_or_none()
+def create(user):
+    name = user.get("name")
+    existing_user = User.query.filter(User.name == name).one_or_none()
 
-    if existing_person is None:
-        new_person = person_schema.load(person, session=db.session)
-        db.session.add(new_person)
+    if existing_user is None:
+        new_user = user_schema.load(user, session=db.session)
+        db.session.add(new_user)
         db.session.commit()
-        return person_schema.dump(new_person), 201
+        return user_schema.dump(new_user), 201
     else:
         abort(406, f"Person with the name {name} already exists")
 
 def read_one(name):
-    person = Person.query.filter(Person.name == name).one_or_none()
+    user = User.query.filter(User.name == name).one_or_none()
 
-    if person is not None:
-        return person_schema.dump(person)
+    if user is not None:
+        return user_schema.dump(user)
     else:
         abort(404, f"Person with the name {name} not found")
 
 def read_all():
-    people = Person.query.all()
-    return person_schema.dump(people)
+    users = User.query.all()
+    return people_schema.dump(users)
 
 
-def update(name, person):
-    existing_person = Person.query.filter(Person.name == name).one_or_none()
+def update(name, user):
+    existing_user = User.query.filter(User.name == name).one_or_none()
 
-    if existing_person:
-        update_person = person_schema.load(person, session=db.session)
-        existing_person.fname = update_person.fname
-        db.session.merge(existing_person)
+    if existing_user:
+        update_person = user_schema.load(user, session=db.session)
+        existing_user.name = update_person.name
+        db.session.merge(existing_user)
         db.session.commit()
-        return person_schema.dump(existing_person), 201
+        return user_schema.dump(existing_user), 201
     else:
         abort(404, f"Person with the name {name} not found")
 
 def delete(name):
-    existing_person = Person.query.filter(Person.lname == name).one_or_none()
+    existing_user = User.query.filter(User.name == name).one_or_none()
 
-    if existing_person:
-        db.session.delete(existing_person)
+    if existing_user:
+        db.session.delete(existing_user)
         db.session.commit()
         return make_response(f"{name} successfully deleted", 200)
     else:
