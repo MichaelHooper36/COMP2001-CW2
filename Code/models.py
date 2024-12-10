2728644233632c5208ab8f9f2b263918721a4940
Michael Hooper <mikeyhooper360@gmail.com>
2024-12-10 10:57:42 +0000

    user sql completion

--- Code/models.py
+++ Code/models.py
@@ -8,16 +8,13 @@ from marshmallow_sqlalchemy import fields
 from config import db, ma
 
 class User(db.Model):
-    __tablename__ = "person"
+    __tablename__ = "users"
+    __table_args__ = {'schema': 'CW2'}
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(63), unique=True)
-    notes = db.relationship(
-        "Note",
-        backref="person",
-        cascade="all, delete, delete-orphan",
-        single_parent=True,
-        order_by="desc(Note.timestamp)"
-    )
+    username = db.Column(db.String(63), unique=True)
+    email = db.Column(db.String(63), unique=True)
+    password = db.Column(db.String(63))
+    role = db.Column(db.String(5))
 
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
